f58b75849f114b77c8cc401d8d4a7619808961ce
Clodoaldo de S. Faria Júnior <61160368+clodoaldocodes@users.noreply.github.com>
2023-06-15 09:24:27 -0300

    create functions rasp

---
+++ raspFunctions.py
@@ -0,0 +1,17 @@
+def takePhotoFCN():
+    print('')
+
+def takeVideoFCN():
+    print('')
+
+def updateModelFCN():
+    print('')
+
+def calibrateFCN():
+    print('')
+
+def runModelFCN():
+    print('')
+
+def breakAllFCN():
+    print('')
