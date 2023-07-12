d7cdce94b1ab063cc75935651405629e04cebd1e
clodoaldocodes <cdsfj@hotmail.com>
2023-07-11 21:35:05 -0300

    #

--- testOS.py
+++ testOS.py
@@ -11,10 +11,18 @@
 # print("CPU Temperature:", temperature_float, "°C")
 
 
-import time
-from picamera import PiCamera
+# import time
+# from picamera import PiCamera
 
-camera = PiCamera()
+# camera = PiCamera()
 
-time.sleep(5)
-camera.capture('/home/pi/v2_prototype/image.jpg')
+# time.sleep(5)
+# camera.capture('/home/pi/v2_prototype/image.jpg')
+
+import cv2
+img = cv2.imread('/home/pi/v2_prototype/drive_img.png')
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+lower_red = (0, 0, 210)
+upper_red = (50, 50, 255) #BGR
+
+mask = cv2.inRange(hsv, lower_red, upper_red)
