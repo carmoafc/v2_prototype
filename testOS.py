dd806ea451f7b26948bd94c65c3d9d0c7fcfec66
clodoaldocodes <cdsfj@hotmail.com>
2023-07-11 21:45:51 -0300

    #

--- testOS.py
+++ testOS.py
@@ -20,9 +20,25 @@
 # camera.capture('/home/pi/v2_prototype/image.jpg')
 
 import cv2
+
 img = cv2.imread('/home/pi/v2_prototype/drive_img.png')
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+#hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 lower_red = (0, 0, 210)
 upper_red = (50, 50, 255) #BGR
 
-mask = cv2.inRange(hsv, lower_red, upper_red)
+mask = cv2.inRange(img, lower_red, upper_red)
+
+contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+for contour in contours:
+    x, y, w, h = cv2.boundingRect(contour)
+    
+    print(cv2.contourArea(contour))
+    if cv2.contourArea(contour) > 0: 
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+xCut = [x, x+w]
+yCut = [y, y+h]
+
+print(xCut)
+print(yCut)
