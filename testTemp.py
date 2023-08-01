921d25e478487cc3b6b6c0c38dd98ff9a3aae21e
clodoaldocodes <cdsfj@hotmail.com>
2023-08-01 16:37:48 -0300

    #

--- testTemp.py
+++ testTemp.py
@@ -3,4 +3,4 @@ import subprocess
 output = subprocess.check_output('/usr/bin/vcgencmd measure_temp', shell=True, text=True, stderr=subprocess.PIPE)
 temperature_str = output.strip().split("=")[1]
 temperature_float = float(temperature_str.split("'")[0])
-print(temperature_float)
+# print(temperature_float)
