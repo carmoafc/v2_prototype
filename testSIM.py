ed51418b9d72522f4838b9487858c0cf8600c7d7
clodoaldocodes <cdsfj@hotmail.com>
2023-08-22 16:56:57 -0300

    #

--- testSIM.py
+++ testSIM.py
@@ -1,33 +1,17 @@
-import time
-from sim800l import SIM800L
+import serial
+import os, time
+import RPi.GPIO as GPIO
 
-sim800l=SIM800L('/dev/serial0')
+GPIO.setmode(GPIO.BOARD)
+port = serial.Serial('/dev/tty1', baudrate=115000, timeout=1)
 
-# Send data and return the first line
-print(sim800l.command("AT+CCLK?\n"))  # ...+CCLK...
+port.write(b'AT\r')
+rcv = port.read(10)
+print(rcv)
+time.sleep(1)
 
-# Same as before, but reading both lines
-sim800l.command("AT+CCLK?\n", lines=0)  # send AT command without reading data
-print("First read line:", sim800l.check_incoming())  # ...+CCLK...
-print("Second read line:", sim800l.check_incoming())  # ...'OK'...
-
-# Same as before, but more elaborated
-sim800l.command("AT+CCLK?\n", lines=0)
-expire = time.monotonic() + 2  # seconds
-sequence = ""
-s = sim800l.check_incoming()
-date = None
-while time.monotonic() < expire:
-    if s[0] == 'GENERIC' and s[1] and s[1].startswith('+CCLK: "'):
-        date = s[1].split('"')[1]
-        sequence += "D"
-    if s == ('OK', None):
-        sequence += "O"
-    if sequence == "DO":
-        print("Date:", date)
-        break
-    time.sleep(0.1)
-    s = sim800l.check_incoming()
-
-if not date:
-    print("Error")
+port.write(b'ATD+5518996847195;\r')
+print('Calling…')
+time.sleep(5)
+port.write(b'ATH\r')
+print('Hang Call…')
