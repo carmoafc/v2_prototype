faa053737260a25e692d7ff053d95e56a972511b
Clodoaldo de S. Faria Júnior <61160368+clodoaldocodes@users.noreply.github.com>
2023-10-24 15:55:25 -0300

    #

--- testImageTago.py
+++ testImageTago.py
@@ -1,10 +1,8 @@
-import requests
-import base64
-import credentials
+from tago import Tago
 
 # Substitua com suas credenciais e informações do dispositivo no Tago
-TAGO_TOKEN = "d0064505-d433-4d6f-a676-79233141f249"
-DEVICE_ID = "64fb28b4801f850010f7a311"
+TAGO_TOKEN = 'd0064505-d433-4d6f-a676-79233141f249'
+DEVICE_ID = '64fb28b4801f850010f7a311'  # Substitua pelo ID real do seu dispositivo
 
 # Caminho da imagem que você deseja enviar
 image_path = "D:/v2_prototype/A_small_cup_of_coffee.JPG"
@@ -12,35 +10,18 @@ image_path = "D:/v2_prototype/A_small_cup_of_coffee.JPG"
 # Função para converter uma imagem em Base64
 def image_to_base64(file_path):
     with open(file_path, 'rb') as image_file:
-        return base64.b64encode(image_file.read()).decode('utf-8')
+        return image_file.read().encode('base64').decode('utf-8')
 
 # Convertendo a imagem em Base64
 image_base64 = image_to_base64(image_path)
 
-# Dados a serem enviados para o Tago
-data_to_send = {
+# Inicializando a instância do Tago com o token
+tago_device = Tago(token=TAGO_TOKEN)
+
+# Enviando dados para o Tago
+tago_device.insert(DEVICE_ID, {
     'variable': 'image_data',
     'value': image_base64,
-}
+})
 
-# URL da API do Tago para enviar dados
-api_url = f'https://api.tago.io/data/{DEVICE_ID}'
-
-# Configurações da requisição para a API do Tago
-headers = {
-    'Content-Type': 'application/json',
-    'Device-Token': TAGO_TOKEN,
-}
-
-# Enviando os dados para o Tago
-response = requests.post(api_url, json=data_to_send, headers=headers)
-
-# Exibindo informações de debug
-print('Status Code:', response.status_code)
-print('Response Text:', response.text)
-
-# Verificando se a requisição foi bem-sucedida
-if response.status_code == 200:
-    print('Dados enviados com sucesso para o Tago:', response.json())
-else:
-    print('Erro ao enviar dados para o Tago. Verifique as mensagens de debug acima.')
+print('Dados enviados com sucesso para o Tago.')
