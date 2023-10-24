1d6731d173a2198d231a1f15ca7213dd965dfbff
Clodoaldo de S. Faria Júnior <61160368+clodoaldocodes@users.noreply.github.com>
2023-10-24 16:21:44 -0300

    #

--- testImageTago.py
+++ testImageTago.py
@@ -1,12 +1,12 @@
-from tago import Device
 import base64
+from tago import Device
 
 # Substitua com suas credenciais e informações do dispositivo no Tago
-TAGO_TOKEN = '40dd5782-129e-4b79-b9f9-5d8ec126f42f'
+DASHBOARD_TOKEN = '40dd5782-129e-4b79-b9f9-5d8ec126f42f'
 DEVICE_ID = 'd0064505-d433-4d6f-a676-79233141f249'  # Substitua pelo ID real do seu dispositivo
 
 # Caminho da imagem que você deseja enviar
-image_path = "D:/v2_prototype/A_small_cup_of_coffee.JPG"
+image_path = 'D:/v2_prototype/A_small_cup_of_coffee.JPG'
 
 # Função para converter uma imagem em Base64
 def image_to_base64(file_path):
@@ -16,16 +16,16 @@ def image_to_base64(file_path):
 # Convertendo a imagem em Base64
 image_base64 = image_to_base64(image_path)
 
-# Inicializando a instância do Device com o token
-tago_device = Device(TAGO_TOKEN)
-
 # Criando um objeto Data com os dados da imagem
 data = {
     'variable': 'image_data',
     'value': image_base64,
 }
 
+# Inicializando a instância do Device com o token do dispositivo
+tago_device = Device(DEVICE_ID)
+
 # Enviando dados para o Tago
 tago_device.insert(data)
 
-print('Dados enviados com sucesso para o Tago.')
+print('Dados enviados com sucesso para o Dashboard no Tago.')
