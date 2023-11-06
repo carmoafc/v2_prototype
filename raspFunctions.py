d755aa00b74e14feac2e25ccf51265cd77d432be
clodoaldocodes <cdsfj@hotmail.com>
2023-11-06 15:27:11 -0300

    #

--- raspFunctions.py
+++ raspFunctions.py
@@ -8,6 +8,7 @@ from email.mime.base import MIMEBase
 from email import encoders
 import credentials
 import numpy as np
+import boto3
 
 def takePhotoFCN(camera):
     import time
@@ -313,3 +314,25 @@ def send_report(client, textFilePath):
         s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
 
         print('Email enviado para ' + email_to_send[i])
+
+    return None
+
+def sendToAws():
+    aws_access_key_id = credentials.aws_access_key_id
+    aws_secret_access_key = credentials.aws_secret_access_key
+    aws_bucket_name = credentials.aws_buket_name
+
+    # Nome do arquivo local que você deseja enviar
+    local_file_path = '/home/pi/v2_prototype/image.jpg'
+
+    # Nome do arquivo no bucket da AWS
+    s3_file_key = 'a'
+
+    # Cria uma instância do cliente S3
+    s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+    # Faz o upload do arquivo para o bucket da AWS
+    s3.upload_file(local_file_path, aws_bucket_name, s3_file_key)
+
+    print(f'A imagem foi enviada para o bucket {aws_bucket_name} com sucesso.')
+    return None
--- testeAWS.py
+++ testeAWS.py
@@ -1,15 +1,21 @@
 import boto3
 import credentials
 
-# Configure AWS credentials
-aws_access_key_id = 
-aws_secret_access_key = 'YOUR_SECRET_KEY'
-aws_session_token = 'YOUR_SESSION_TOKEN'  # Optional for temporary credentials
+# Substitua essas informações pelas suas próprias credenciais e nome do bucket
+aws_access_key_id = credentials.aws_access_key_id
+aws_secret_access_key = credentials.aws_secret_access_key
+aws_bucket_name = credentials.aws_buket_name
 
-s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, aws_session_token=aws_session_token)
+# Nome do arquivo local que você deseja enviar
+local_file_path = '/home/pi/v2_prototype/A_small_cup_of_coffee.JPG'
 
-bucket_name = 'your-s3-bucket-name'
-file_path = 'path/to/your/file.ext'  # Path to the file you want to upload
-s3_key = 'path/in-s3/bucket/file.ext'  # The key (path) under which the file will be stored in S3
+# Nome do arquivo no bucket da AWS
+s3_file_key = 'a'
 
-s3.upload_file(file_path, bucket_name, s3_key)
+# Cria uma instância do cliente S3
+s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+# Faz o upload do arquivo para o bucket da AWS
+s3.upload_file(local_file_path, aws_bucket_name, s3_file_key)
+
+print(f'A imagem foi enviada para o bucket {aws_bucket_name} com sucesso.')
